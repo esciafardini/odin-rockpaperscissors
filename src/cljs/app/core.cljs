a51a84d1d58aae7b9cfb1127431c2c26a47c9611
esciafardini <edward.ciafardini@aclaimant.com>
2023-03-23 13:02:57 -0400

    Simplify data model for game

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,107 +1,83 @@
 (ns app.core
   (:require
    [clojure.set :as set]
-   [clojure.string :as string]))
+   [clojure.string :as string]
+   [cljs.core :as c]))
 
 (def log (.-log js/console))
 
-(def choices
-  {0 "rock"
-   1 "paper"
-   2 "scissors"})
+(def computer-choices
+  {0 :rock
+   1 :paper
+   2 :scissors})
 
-(def end-of-round-output
-  {"rock" "Rock beats scissors."
-   "paper" "Paper beats rock."
-   "scissors" "Scissors beats paper."
-   "tie" "Tie game."
-   "error" "Invalid user input."})
+(def ui-messages
+  {:rock "Rock beats scissors."
+   :paper "Paper beats rock."
+   :scissors "Scissors beats paper."
+   :tie "Tie game."
+   :error "Invalid user input."})
 
-; rock beats scissors
-; paper beats
-
-(def test-data
-  ; how the data should/could look to simplify things
-  [{:player :computer
-    :choice "rock"}
-   {:player :user
-    :choice "rock"}])
-
-(defn new-determine
+(defn determine-winner
   "If there is a winner, a map will be returned.
    Otherwise, a keyword that signifies bad user input (:error) or a tie game (:tie)"
   [data]
-  (let [choices (set (map :choice data))
-        valid-entry? (set/subset? choices #{"rock" "paper" "scissors"})
-        return-winner-info (fn [selection game-data]
-                             (->> game-data
-                                  (filter (comp #{selection} :choice))
-                                  first))]
+  (let [choices (set (vals data))
+        valid-entry? (set/subset? choices #{:rock :paper :scissors})
+        return-winner (fn [selection game-data]
+                        (-> game-data
+                            set/map-invert
+                            (get selection)))]
     (cond
-      (not valid-entry?) :error
-      (= 1 (count choices)) :tie
-      (= choices #{"rock" "paper"}) (return-winner-info "paper" data)
-      (= choices #{"rock" "scissors"}) (return-winner-info "rock" data)
-      (= choices #{"scissors" "paper"}) (return-winner-info "scissors" data))))
+      (not valid-entry?)
+      {:winner :error
+       :ui-message (get ui-messages :error)}
 
-(new-determine test-data)
+      (= 1 (count choices))
+      {:winner :tie
+       :ui-message (get ui-messages :tie)}
 
-(->> test-data
-     (filter (comp #{"paper"} :choice))
-     first
-     :player)
+      (= choices #{:rock :paper})
+      {:winner (return-winner :paper data)
+       :ui-message (get ui-messages :paper)}
 
+      (= choices #{:rock :scissors})
+      {:winner (return-winner :rock data)
+       :ui-message (get ui-messages :rock)}
 
-
-
-
-
-
-
-(defn determine-winner [player-selection winning-selection]
-  (if (= player-selection winning-selection)
-    (do
-      (log (str (get end-of-round-output winning-selection) " Player wins!"))
-      {:winner :player})
-    (do
-      (log (str (get end-of-round-output winning-selection) " Computer wins!"))
-      {:winner :computer})))
+      (= choices #{:scissors :paper})
+      {:winner (return-winner :scissors data)
+       :ui-message (get ui-messages :scissors)})))
 
 (defn get-computer-choice []
   (let [n (rand-int 3)]
-    (get choices n)))
+    (get computer-choices n)))
 
-(defn play [player-selection-raw computer-selection]
-  (let [player-selection (string/lower-case player-selection-raw)]
-    (cond
-      (not (seq (#{"rock" "paper" "scissors"} player-selection)))
-      (do (log (get end-of-round-output "error"))
-          {:winner :error})
-      (= computer-selection player-selection)
-      (do (log (get end-of-round-output "tie"))
-          {:winner :tie})
-      (= #{player-selection computer-selection} #{"rock" "scissors"})
-      (determine-winner player-selection "rock")
-      (= #{player-selection computer-selection} #{"rock" "paper"})
-      (determine-winner player-selection "paper")
-      (= #{player-selection computer-selection} #{"paper" "scissors"})
-      (determine-winner player-selection "scissors"))))
+(defn play [user-input]
+  (let [user-selection (keyword (string/lower-case user-input))
+        computer-selection (get-computer-choice)
+        game-info {:user user-selection
+                   :computer computer-selection}]
+    ;; log the game
+    #_(println game-info)
+    (log (clj->js (determine-winner game-info)))
+    (determine-winner game-info)))
 
 (defn game []
   (loop [game-count 0
          computer-score 0
-         player-score 0]
+         user-score 0]
     (if (= game-count 5)
-      (log (str "Player: " player-score ", Computer: " computer-score))
-      (case (:winner (play (js/prompt "Rock Paper Scissors") (get-computer-choice)))
+      (log (str "User: " user-score ", Computer: " computer-score))
+      (case (:winner (play (js/prompt "Rock Paper Scissors")))
         :tie
-        (recur game-count computer-score player-score)
+        (recur game-count computer-score user-score)
         :error
-        (recur game-count computer-score player-score)
-        :player
-        (recur (inc game-count) computer-score (inc player-score))
+        (recur game-count computer-score user-score)
+        :user
+        (recur (inc game-count) computer-score (inc user-score))
         :computer
-        (recur (inc game-count) (inc computer-score) player-score)))))
+        (recur (inc game-count) (inc computer-score) user-score)))))
 
 (defn ^:export init []
   (game))
