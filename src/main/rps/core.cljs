67c482b579562aa9c7398426d2bd37dcd704b98b
esciafardini <edward.ciafardini@aclaimant.com>
2023-03-31 14:10:49 -0400

    Refine gameplay, add placeholders for graphics

--- src/main/rps/core.cljs
+++ src/main/rps/core.cljs
@@ -1,8 +1,9 @@
 (ns rps.core
   (:require
+   [cljs.core :as c]
    [clojure.set :as set]
-   [rps.js-helpers :refer [console-log get-element-by-id set-text-content!]]
-   [cljs.core :as c]))
+   [clojure.string :as string]
+   [rps.js-helpers :refer [console-log get-element-by-id set-text-content!]]))
 
 (def computer-choices
   {0 :rock
@@ -10,11 +11,11 @@
    2 :scissors})
 
 (def ui-messages
-  {:rock "Rock beats scissors."
-   :paper "Paper beats rock."
-   :scissors "Scissors beats paper."
-   :tie "Tie game."
-   :error "Invalid user input."})
+  {:rock "Rock beats scissors"
+   :paper "Paper beats rock"
+   :scissors "Scissors beats paper"
+   :tie "Tie game"
+   :error "Invalid user input"})
 
 (defn get-winner
   "Returns keyword of :user or :computer based on winning-selection"
@@ -50,7 +51,6 @@
       (= choices #{:scissors :paper})
       (outcome :scissors data))))
 
-
 (defn play-round [user-selection computer-selection]
   (let [game-info {:user user-selection
                    :computer computer-selection}]
@@ -66,26 +66,37 @@
   (let [n (rand-int 3)]
     (get computer-choices n)))
 
-(defn game-over []
+(defn game-over [winner-div]
   (doseq [button-element button-elements]
-    (set! (.. button-element -disabled) true))
-  (set-text-content! "game-over-message" "Game over!!!!"))
+    (set! (.. button-element -disabled) true)
+    (.remove button-element))
 
+  (let [winner (if (= winner-div "user-score")
+                 "PLAYER"
+                 "COMPUTER")]
+    (set! (.. (get-element-by-id "ui-message") -style -fontWeight) "bold")
+    (set-text-content! "ui-message" (str winner " WINS"))))
 
-(defn victory-fx [score div]
+(defn victory-fx [score div-id]
   (swap! score inc)
   (if (= @score 5)
     (do
-      (set-text-content! div (str @score " (Winner)"))
-      (game-over))
-    (set-text-content! div @score)))
+      (console-log div-id)
+      (console-log (= div-id "user-score"))
+      (console-log (= div-id "computer-score"))
+      (set-text-content! div-id @score)
+      (game-over div-id))
+    (set-text-content! div-id @score)))
 
 (def button-types ["rock" "paper" "scissors"])
 
 (doseq [button-type button-types]
   (.addEventListener (get-element-by-id button-type) "click"
                      #(let [{:keys [ui-message winner]} (play-round (keyword button-type) (get-computer-choice!))]
-                        (set-text-content! "ui-message" ui-message)
+                        (set-text-content! "ui-message" (str ui-message " " (case winner
+                                                                              :user "- You win this round"
+                                                                              :computer "- Computer wins this round"
+                                                                              :tie nil)))
                         (case winner
                           :computer (victory-fx computer-score "computer-score")
                           :user (victory-fx user-score "user-score")
