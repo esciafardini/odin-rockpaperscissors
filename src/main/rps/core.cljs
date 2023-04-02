e34c3b6c9f90f4593328f42cfc53173501434aba
esciafardini <edward.ciafardini@aclaimant.com>
2023-04-02 15:43:36 -0400

    Cleanup of code

--- src/main/rps/core.cljs
+++ src/main/rps/core.cljs
@@ -1,8 +1,9 @@
 (ns rps.core
   (:require
-   [cljs.core :as c]
-   [clojure.set :as set]
    [rps.js-helpers :refer [console-log get-element-by-id set-text-content!]]))
+
+(def player-score (atom 0))
+(def computer-score (atom 0))
 
 (def computer-choices
   {0 :rock
@@ -15,91 +16,67 @@
    :scissors "Scissors beats paper"
    :tie "Tie game"})
 
-(defn get-winner
-  "Returns keyword of :player or :computer based on winning-selection"
-  [winning-selection game-data]
-  (-> game-data
-      set/map-invert
-      (get winning-selection)))
-
-(defn get-ui-message
-  "Returns UI output for game"
-  [selection]
-  (get ui-messages selection))
-
-(defn outcome [selection game-data]
-  {:winner (get-winner selection game-data)
-   :ui-message (get-ui-message selection)})
+(defn return-game-info [winner rps-selection-map]
+  (merge rps-selection-map {:winner (get rps-selection-map winner)
+                            :ui-message (get ui-messages winner)}))
 
 (defn determine-winner
   "If there is a winner, a map will be returned.
    Otherwise, a keyword that signifies tie game (:tie)"
-  [data]
-  (let [choices (set (vals data))]
-    (cond
-      (= 1 (count choices))
-      {:winner :tie :ui-message (get-ui-message :tie)}
+  [rps-selection-map]
+  (let [choices (set (vals (select-keys rps-selection-map [:player-selection :computer-selection])))]
+    (cond->> rps-selection-map
+      (= (count choices) 1)
+      (return-game-info :tie)
 
       (= choices #{:rock :paper})
-      (outcome :paper data)
+      (return-game-info :paper)
 
       (= choices #{:rock :scissors})
-      (outcome :rock data)
+      (return-game-info :rock)
 
       (= choices #{:scissors :paper})
-      (outcome :scissors data))))
+      (return-game-info :scissors))))
 
 (defn play-round [player-selection computer-selection]
-  (let [game-info {:player player-selection
-                   :computer computer-selection}]
-    (merge game-info (determine-winner game-info))))
+  (let [rps-selection-map {:player-selection player-selection
+                           :computer-selection computer-selection}]
 
-(def button-elements
-  (.querySelectorAll js/document "button"))
-
-(def player-score (atom 0))
-(def computer-score (atom 0))
+    (if (= computer-selection player-selection)
+      (determine-winner rps-selection-map)
+      (determine-winner (merge rps-selection-map {player-selection :player ;inverting for lookup (if winner = scissors, who won?)
+                                                  computer-selection :computer})))))
 
 (defn get-computer-choice! []
   (let [n (rand-int 3)]
     (get computer-choices n)))
 
+(defn set-text-and-images! [computer-selection player-selection score div-id]
+  (set! (.. (get-element-by-id "computer-img") -src) (str "public/" (name computer-selection) ".png"))
+  (set! (.. (get-element-by-id "player-img") -src) (str "public/" (name player-selection) ".png"))
+  (and div-id (set-text-content! div-id score)))
+
 (defn game-over [winner-div]
-  (doseq [button-element button-elements]
+  (doseq [button-element (.querySelectorAll js/document "button")]
     (set! (.. button-element -disabled) true)
     (set! (.. button-element -style -visibility) "hidden"))
+  (set-text-content! "ui-message" (if (= winner-div "player-score") "YOU WIN" "YOU LOSE")))
 
-  (let [game-over-message (if (= winner-div "player-score")
-                            "YOU WIN"
-                            "YOU LOSE")]
-    (set! (.. (get-element-by-id "ui-message") -style -fontWeight) "bold")
-    (set-text-content! "ui-message" game-over-message)))
-
-(defn victory-fx [score div-id computer player]
+(defn victory-fx [computer-selection player-selection score div-id]
   (and score (swap! score inc))
-  (if (and score (= @score 5))
+  (if (= @score 5)
     (do
-      (set! (.. (get-element-by-id "computer-img") -src) (str "public/" (name computer) ".png"))
-      (set! (.. (get-element-by-id "player-img") -src) (str "public/" (name player) ".png"))
-      (set-text-content! div-id @score)
+      (set-text-and-images! computer-selection player-selection @score div-id)
       (game-over div-id))
-    (do
-      (set! (.. (get-element-by-id "computer-img") -src) (str "public/" (name computer) ".png"))
-      (set! (.. (get-element-by-id "player-img") -src) (str "public/" (name player) ".png"))
-      (and div-id (set-text-content! div-id @score)))))
+    (set-text-and-images! computer-selection player-selection @score div-id)))
 
-(def button-types ["rock" "paper" "scissors"])
-
-(doseq [button-type button-types]
+(doseq [button-type ["rock" "paper" "scissors"]]
   (.addEventListener (get-element-by-id button-type) "click"
-                     #(let [{:keys [player computer ui-message winner]} (play-round (keyword button-type) (get-computer-choice!))]
-                        (set! (.. (get-element-by-id "computer-selection") -style -visibility) "visible")
-                        (set! (.. (get-element-by-id "player-selection") -style -visibility) "visible")
+                     #(let [{:keys [player-selection computer-selection ui-message winner]} (play-round (keyword button-type) (get-computer-choice!))]
                         (set-text-content! "ui-message" ui-message)
                         (case winner
-                          :computer (victory-fx computer-score "computer-score" computer player)
-                          :player (victory-fx player-score "player-score" computer player)
-                          :tie (victory-fx nil nil computer player)))))
+                          :computer (victory-fx computer-selection player-selection computer-score "computer-score")
+                          :player (victory-fx computer-selection player-selection player-score "player-score")
+                          (victory-fx computer-selection player-selection (atom nil) nil)))))
 
-(defn init []
-  (console-log "PLAY!!!"))
+(defn init [])
